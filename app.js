9e5a0fc647fc47f2233e1b52db1ea746928624af
ening <ening6649@yahoo.com>
2021-11-20 15:30:42 -0500

    first input

---
+++ app.js
@@ -0,0 +1,20 @@
+// console.log('Hello Node!');
+// captures whatever is typed into command line . space between creates a new line in command line
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    // This...
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+      console.log(profileDataArr[i]);
+    }
+  
+    console.log('================');
+  
+    // Is the same as this...
+    profileDataArr.forEach((profileItem) => {
+      console.log(profileItem)
+    });
+  };
