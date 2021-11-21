6da4fc7001c9351fb1962376648b382d4e9b71ab
ening <ening6649@yahoo.com>
2021-11-21 10:22:22 -0500

    fs

--- app.js
+++ app.js
@@ -2,7 +2,12 @@
 // captures whatever is typed into command line . space between creates a new line in command line
 // var commandLineArgs = process.argv;
 // console.log(commandLineArgs);
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// slice into two parts at the point of process.arg.length
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const name = profileDataArgs[0];
+const github = profileDataArgs[1];
+
+
 // console.log(profileDataArgs);
 
 // const printProfileData = profileDataArr => {
@@ -19,18 +24,67 @@
 //     });
 //   };
 
-const profileDataArgs = process.argv.slice(2);
+// const generatePage = () => 'Name: Jane, Github: janehub';
+// console.log(generatePage())
+// const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+// console.log(generatePage('Jane', 'janehub'));
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+// const generatePage = (userName, githubName) => {
+//     return `
+//       Name: ${userName}
+//       GitHub: ${githubName}
+//     `;
+//   };
 
-  console.log('================');
+// console.log(generatePage('Jane', 'janehub'));
+// console.log(generatePage(name, github));
+// console.log(name, github);
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
+// load file system fs.close() fs.open() fs.readFile() fs.writeFile()
+const fs = require('fs');
+// const http = require('http')
+// const url=require('url')
+
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
 };
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
+// console.log(generatePage('Jane', 'janehub'));
+
+const textMessage = fs.readFile('./mesasge.txt','utf8',(err,data)=>{
+    if (err) {
+        console.log(err);
+    }
+    console.log(data);
+});
+// running node index.js should now read text in ./message.txt
+
+const add = (a,b) =>a + b;
+const multiply = (a,b) =>a*b;
+
+module.exports={
+    add:add,
+    multiply:multiply,
+}
+// use const math = require("./math");  to load a js file with the above. use the below console log in main js to load math
+console.log(math.add(2,3));
