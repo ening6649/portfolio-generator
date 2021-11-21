ee5911d48c882e6c5bc31bf5501b34a641b9158d
ening <ening6649@yahoo.com>
2021-11-21 14:06:29 -0500

    added inquirer

--- app.js
+++ app.js
@@ -45,20 +45,6 @@
 // const http = require('http')
 // const url=require('url')
 
-const fs = require('fs');
-const generatePage = require('./src/page-template.js')
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
 
 // const textMessage = fs.readFile('./mesasge.txt','utf8',(err,data)=>{
 //     if (err) {
@@ -77,3 +63,29 @@ fs.writeFile('./index.html', generatePage(name, github), err => {
 // }
 // use const math = require("./math");  to load a js file with the above. use the below console log in main js to load math
 // console.log(math.add(2,3));
+
+
+
+// start code 
+const inquirer = require('inquirer')
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template')
+
+// const pageHTML = generatePage(name, github);
+
+
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw new Error(err);
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
